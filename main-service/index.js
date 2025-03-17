2ed3c8c1be386994e019247fcca76eeba3c5c14f
nimapdevyash <yash.sawarkar@nimapinfotech.com>
2025-03-17 17:52:40 +0530

    flow updated

--- main-service/index.js
+++ main-service/index.js
@@ -7,58 +7,8 @@ app.get("/", (req, res) => {
   res.send(
     ` 
       <h1>This is the Main service</h1>
-
-      <h2>Main service Endpoints</h2>
-      <ul>
-        <li>
-          <a href="http://localhost:3000/users"> Users main service</a>
-        </li>
-        <li>
-          <a href="http://localhost:3000/products"> Products main service </a>
-        </li>
-      </ul>
-
-      <h2>Micro services</h2>
-      <ul>
-        <li>
-          <a href="http://localhost:3002/users"> Users service </a>
-        </li>
-        <li>
-          <a href="http://localhost:3001/products"> Products service </a>
-        </li>
-      </ul>
     `
   );
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const response = await axios.get("http://localhost:3002/users");
-    res.status(200).json({
-      message: "main service",
-      data: response.data?.data,
-    });
-  } catch (error) {
-    res.status(500).send({
-      message: "internal error",
-      error,
-    });
-  }
-});
-
-app.get("/products", async (req, res) => {
-  try {
-    const response = await axios.get("http://localhost:3001/products");
-    res.status(200).json({
-      message: "main service",
-      data: response.data?.data,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "internal error",
-      error,
-    });
-  }
 });
 
 app.listen(3000, () => console.log("main app is live on 3000"));
